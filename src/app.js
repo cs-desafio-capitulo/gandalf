cebc57119c45b3ccb67a29527b5c899cbe51409b
Rosa Bugan <rosita.bugan@gmail.com>
2018-01-09 09:36:21 -0200

    enable cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import routes from './routes';
 import database from './config/database';
@@ -7,10 +8,10 @@ import logger from './services/logger';
 const app = express();
 
 const configureExpress = () => {
+  app.use(cors());
   app.use(bodyParser.json());
   app.use('/', logger, routes);
   return app;
 };
-
 
 export default () => database.connect().then(configureExpress);
